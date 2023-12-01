e15b1e689a7bcb7e638acb4b208061c85618adbe
DarenskyRoman <81371645+DarenskyRoman@users.noreply.github.com>
2023-12-01 23:54:07 +0400

    Update 1_🏠_Homepage.py

--- 1_🏠_Homepage.py
+++ 1_🏠_Homepage.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import subprocess
 
 st.set_page_config(
     page_title="Home"
@@ -14,3 +15,5 @@ st.write("""
          
                 Current supported languages: Russian. 
             """)
+
+print(subprocess.run(["model_load.sh"]))
