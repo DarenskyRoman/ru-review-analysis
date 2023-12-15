cd5f98cb6c6769fb101dddb4262a49c16e00818a
DarenskyRoman <81371645+DarenskyRoman@users.noreply.github.com>
2023-12-16 02:02:31 +0400

    Update and rename 1_🏠_Homepage.py to main.py

---
+++ main.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import torch
+import re
+from pymystem3 import Mystem
+from transformers import AutoModelForSequenceClassification
+from transformers import BertTokenizerFast
+from nltk import word_tokenize
+
+
+@torch.no_grad()
+def predict(text):
+    inputs = tokenizer(text, max_length=512, padding=True, truncation=True, return_tensors='pt')
+    outputs = model(**inputs)
+    predicted = torch.nn.functional.softmax(outputs.logits, dim=1)
+    predicted = torch.argmax(predicted, dim=1).numpy()
+    return predicted
+
+def text_prepocessing(text):
+
+    text = text.lower()
+
+    text = re.sub(r'[^\u0400-\u04FF]', ' ', text)
+
+    text_tokens = word_tokenize(text)
+
+    text_tokens = [stem.lemmatize(token)[0] for token in text_tokens]
+    text = ' '.join(text_tokens)
+
+    return text
+    
+
+def do_analysis(text):
+
+    if text == '' or text == None:
+        return "Ждём ввод 🕒"
+
+    result = predict(text_prepocessing(text))
+    
+    if result == 1:
+        result = "Положительно 🙂"
+    if result == 0:
+        result = "Нейтрально 😐"
+    if result == 2:
+        result = "Отрицательно ☹️"
+
+    return result
+
+stem = Mystem()
+
+tokenizer = BertTokenizerFast.from_pretrained('blanchefort/rubert-base-cased-sentiment-rurewiews')
+model = AutoModelForSequenceClassification.from_pretrained('blanchefort/rubert-base-cased-sentiment-rurewiews', return_dict=True)
+
+st.title("Анализ тональности отзывов о товарах")
+
+st.write("""
+        Введите отзыв в поле ниже и увидьте его тональность.
+         
+        Программа работает для отзывов, написанных на русском языке.
+        """)
+
+text = st.text_area(label="Я поле ниже", placeholder="Отзыв сюда")
+
+st.write(f"##### Результат: {do_analysis(text)}")
